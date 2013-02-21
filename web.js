45e83d5d59a60da53a437822a9333ab4fd11bdab
Niklas <andreasson.n@gmail.com>
2013-02-21 16:38:46 +0100

    Initial commit

--- web.js
+++ web.js
@@ -6,16 +6,28 @@ var app = express.createServer();
 
 var getDishes = function(window, restaurantId) {
   var dishes = [];
-  window.$('#' + restaurantId + ' li').each(function() {
+  window.$('#' + restaurantId + ' li:not(#notitle)').each(function() {
       var $li = window.$(this)
         , title = $li.find('span.title')[0].innerHTML
-        , desc = $li.find('span.desc')[0].innerHTML.replace(/@\d+$/, '');
+        , desc = $li.find('span.desc')[0].innerHTML.replace(/@(\d+)?$/, '');
 
       dishes.push({
         title: title
       , desc: desc
       });
     });
+
+    window.$('#' + restaurantId + ' li#notitle').each(function() {
+      var $li = window.$(this)
+        , title = ''
+        , desc = $li.find('span.desc')[0].innerHTML.replace(/@(\d+)?$/, '');
+
+      dishes.push({
+        title: title
+      , desc: desc
+      });
+    });
+
   return dishes;
 };
 
@@ -30,7 +42,7 @@ app.get('/:place', function(request, response) {
     res.on('end', function() {
       jsdom.env(data, ['http://code.jquery.com/jquery.js'], function(errors, window) {
         var unionRestaurant = getDishes(window, 'K')
-          , linsen = getDishes(window, 'L');
+          , linsen = getDishes(window, 'Li');
 
         var restaurants = {
           karen: {
