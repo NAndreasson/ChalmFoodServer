ce3cfbfaf403aff9fb710bdbbc25a415295600a2
Niklas Andreasson <eaglus_@hotmail.com>
2013-12-06 23:42:58 +0100

    Separated code, and added a 204 response if the url specified does not contain a campus

---
+++ dishRetriever.js
@@ -0,0 +1,39 @@
+var request = require('request')
+  , FeedParser = require('feedparser')
+  , moment = require('moment')
+  ;
+
+exports.getDishes = getDishes;
+
+function getDishes(restaurant, cb) {
+  var restaurantFeedUrl = buildFeedUrl( restaurant.url )
+    , dishes = []
+    ;
+
+  request( restaurantFeedUrl ).pipe( new FeedParser() )
+      .on('error', function(err) {
+        cb(err);
+      })
+      .on('readable', function() {
+        var stream = this
+          , item
+          , desc
+          ;
+
+        while ( item = stream.read() ) {
+          desc = item.description;
+          // add dish (and remove the @ for price from description)
+          dishes.push({ title: item.title, desc: desc.slice( 0, desc.lastIndexOf('@') ) });
+        }
+      })
+      .on('end', function() {
+        restaurant.dishes = dishes;
+        cb(null, restaurant);
+      });
+}
+
+function buildFeedUrl(baseUrl) {
+  var todaysDate = moment().format('YYYY-MM-DD');
+
+  return baseUrl + todaysDate + '.rss';
+}
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var express = require('express')
   , restaurants = require('./restaurants')
-  , request = require('request')
-  , FeedParser = require('feedparser')
+  , dishRetriever = require('./dishRetriever')
   , async = require('async')
-  , moment = require('moment')
   , app = express()
   ;
 
 
 app.get('/:campus', function(req, res) {
-  // which campus?
   var campus = req.params.campus;
 
-  // TODO error handling, what if the restaurant dont exist
   var campusRestaurants = restaurants[ campus ];
 
+  if ( !campusRestaurants ) {
+    res.send( 204 );
+    return;
+  }
+
   // get dishes for restaurants at campus
-  async.map(campusRestaurants, getDishes, function(err, results) {
+  async.map(campusRestaurants, dishRetriever.getDishes, function(err, results) {
     if (err) {
       return;
     }
@@ -25,38 +26,6 @@ app.get('/:campus', function(req, res) {
   });
 });
 
-function getDishes(restaurant, cb) {
-  var restaurantFeedUrl = buildFeedUrl( restaurant.url )
-    , dishes = []
-    ;
-
-  request( restaurantFeedUrl ).pipe( new FeedParser() )
-      .on('error', function(err) {
-        cb(err);
-      })
-      .on('readable', function() {
-        var stream = this
-          , item
-          , desc
-          ;
-
-        while ( item = stream.read() ) {
-          desc = item.description;
-          // add dish (and remove the @ for price from description)
-          dishes.push({ title: item.title, desc: desc.slice( 0, desc.lastIndexOf('@') ) });
-        }
-      })
-      .on('end', function() {
-        restaurant.dishes = dishes;
-        cb(null, restaurant);
-      });
-}
-
-function buildFeedUrl(baseUrl) {
-  var todaysDate = moment().format('YYYY-MM-DD');
-
-  return baseUrl + todaysDate + '.rss';
-}
 
 
 var port = process.env.PORT || 5000;
